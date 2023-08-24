0fca076264e66e074135e68afa09483a0bb3d75e
Julianms11 <juliandavidms2004@gmail.com>
2023-08-23 23:38:54 -0500

    equivalencias

--- equivalencias.py
+++ equivalencias.py
@@ -1,4 +1,11 @@
 import numpy as np
 
-def paralelo(a):
-    return 0
+def paralelo(Z: list):
+    Imps = np.ones(len(Z), dtype=complex)
+    for i in range(len(Z)):
+        Imps[i] = 1/Z[i]
+    ImpPar = 1/np.sum(Imps)
+    return ImpPar
+
+def serie(Z: list):
+    return np.sum(Z)
